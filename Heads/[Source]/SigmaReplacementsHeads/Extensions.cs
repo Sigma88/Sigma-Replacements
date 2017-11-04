ade61bd0376fe54269681266397bfee4cb47aa2b
Sigma88 <sigmamodexpansions@outlook.com>
2017-11-04 11:36:22 +0100

    unused usings

--- Heads/[Source]/SigmaReplacementsHeads/Extensions.cs
+++ Heads/[Source]/SigmaReplacementsHeads/Extensions.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
 using UnityEngine;
-using KSP.UI;
-using KSP.UI.Screens;
-using KSP.UI.TooltipTypes;
 
 
 namespace SigmaReplacements
