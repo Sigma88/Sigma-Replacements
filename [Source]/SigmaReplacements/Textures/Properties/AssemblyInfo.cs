a1205cdc5ec016ee69a29b53e450b6de3d4ff16a
Sigma88 <sigmamodexpansions@outlook.com>
2017-11-05 19:37:46 +0100

    Textures

---
+++ [Source]/SigmaReplacements/Textures/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("SigmaReplacementsTextures")]
+[assembly: AssemblyDescription("Sigma Replacements: Textures")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Sigma88")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Copyright (C) Sigma88 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: KSPAssembly("SigmaReplacementsTextures", 1, 0)]
+[assembly: AssemblyVersion("1.0.0")]
