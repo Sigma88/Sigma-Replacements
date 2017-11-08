c07db675650975687d63048d970eca1b33503e9c
Sigma88 <sigmamodexpansions@outlook.com>
2017-11-08 02:36:58 +0100

    fix loader

--- [Source]/SigmaReplacements/Heads/SettingsLoader.cs
+++ [Source]/SigmaReplacements/Heads/SettingsLoader.cs
@@ -16,14 +16,17 @@ namespace SigmaReplacements
                 for (int i = 0; i < InfoNodes?.Length; i++)
                 {
                     ConfigNode[] requirements = InfoNodes[i].GetNodes("Requirements");
-                    ConfigNode head = InfoNodes[i].GetNode("Head");
+                    ConfigNode[] heads = InfoNodes[i].GetNodes("Head");
 
                     if (requirements.Length == 0)
                         requirements = new[] { new ConfigNode() };
 
                     for (int j = 0; j < requirements.Length; j++)
                     {
-                        HeadInfo.List.Add(new HeadInfo(requirements[j], head));
+                        for (int k = 0; k < heads.Length; k++)
+                        {
+                            HeadInfo.List.Add(new HeadInfo(requirements[j], heads[k]));
+                        }
                     }
                 }
 
