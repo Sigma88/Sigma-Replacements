00d3aac0970ec2652f500a8765210f9d5688272d
Sigma88 <sigmamodexpansions@outlook.com>
2017-11-29 17:34:54 +0100

    load also DXT5

--- [Source]/SigmaReplacements/Common/Extensions.cs
+++ [Source]/SigmaReplacements/Common/Extensions.cs
@@ -355,20 +355,41 @@ namespace SigmaReplacements
         
         internal static Texture2D ToDDS(this byte[] bytes)
         {
-            if (bytes[4] != 124) return null; //this byte should be 124 for DDS image files
+            try
+            {
+                if (bytes[4] != 124) return null; //this byte should be 124 for DDS image files
 
-            int height = bytes[13] * 256 + bytes[12];
-            int width = bytes[17] * 256 + bytes[16];
+                int height = bytes[13] * 256 + bytes[12];
+                int width = bytes[17] * 256 + bytes[16];
 
-            int header = 128;
-            byte[] data = new byte[bytes.Length - header];
-            Buffer.BlockCopy(bytes, header, data, 0, bytes.Length - header);
+                int header = 128;
+                byte[] data = new byte[bytes.Length - header];
+                Buffer.BlockCopy(bytes, header, data, 0, bytes.Length - header);
 
-            Texture2D texture = new Texture2D(width, height, TextureFormat.DXT1, false);
-            texture.LoadRawTextureData(data);
-            texture.Apply();
+                TextureFormat format;
+                switch ((double)height * width / data.Length)
+                {
+                    case 1d:
+                        format = TextureFormat.DXT5;
+                        break;
+                    case 2d:
+                        format = TextureFormat.DXT1;
+                        break;
+                    default:
+                        return null;
+                }
 
-            return (texture);
+                Texture2D texture = new Texture2D(width, height, format, false);
+
+                texture.LoadRawTextureData(data);
+                texture.Apply();
+
+                return (texture);
+            }
+            catch
+            {
+                return null;
+            }
         }
     }
 }
