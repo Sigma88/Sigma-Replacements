0a1ece7be8ca2265db860c9811554990b3554b92
Sigma88 <sigmamodexpansions@outlook.com>
2019-01-07 16:53:26 +0100

    fix usechance

--- [Source]/SigmaReplacements/Heads/CustomHead.cs
+++ [Source]/SigmaReplacements/Heads/CustomHead.cs
@@ -72,7 +72,7 @@ namespace SigmaReplacements
                     {
                         if (string.IsNullOrEmpty(collection) || collection == info.collection)
                         {
-                            if (info.useChance != 1)
+                            if (useChance == null && info.useChance != 1)
                                 useChance = kerbal.Hash(info.useGameSeed) % 100;
 
                             Debug.Log("CustomHead.LoadFor", "Matching head useChance = " + info.useChance + " to generated chance = " + useChance + " %");
