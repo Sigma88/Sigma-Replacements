6a1496a6f8f2524b26dd80fd248767622820d527
Sigma88 <sigmamodexpansions@outlook.com>
2020-02-03 23:05:37 +0100

    Add new Extensions to get the mesh

---
+++ [Source]/SigmaReplacements/MenuScenes/Utility/Extensions.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using Kopernicus.Components.ModularScatter;
+
+
+namespace SigmaReplacements
+{
+    namespace MenuScenes
+    {
+        internal static class Extensions
+        {
+            internal static Mesh GetModularScatterMesh(this CelestialBody planet, PQSLandControl.LandClassScatter scatter)
+            {
+                Mesh mesh = null;
+
+                if (scatter != null)
+                {
+                    if (scatter.baseMesh == null || scatter.baseMesh.name == "Kopernicus-CubeDummy")
+                    {
+                        ModularScatter[] scatters = planet.GetComponentsInChildren<ModularScatter>(true);
+
+                        for (int i = 0; i < scatters?.Length; i++)
+                        {
+                            ModularScatter modularScatter = scatters[i];
+
+                            if (scatter == modularScatter?.scatter)
+                            {
+                                return modularScatter.baseMesh;
+                            }
+                        }
+                    }
+                }
+
+                return mesh;
+            }
+        }
+    }
+}
