03591a8b78e71505a386538f69a56aeacd48c920
Sigma88 <sigmamodexpansions@outlook.com>
2020-02-17 15:21:29 +0100

    LightTracker

--- [Source]/SigmaReplacements/MenuScenes/MenuScenes/MenuLight.cs
+++ [Source]/SigmaReplacements/MenuScenes/MenuScenes/MenuLight.cs
@@ -30,17 +30,7 @@ namespace SigmaReplacements
             internal float? shadowStrength = null;
 
             // Movements
-            internal float? rotatoSpeed = null;
-
-            internal string pivotAround = null;
-            internal Vector3? pivotPosition = null;
-            internal Quaternion? pivotRotation = null;
-            internal Vector3? pivotScale = null;
-            internal float? pivotDistance = null;
-            internal float? pivotRotatoSpeed = null;
-
-            internal float? bobberSeed = null;
-            internal Vector3? bobberOFS = null;
+            internal bool? trackCamera = null;
 
 
             // New MenuLight from cfg
@@ -80,17 +70,7 @@ namespace SigmaReplacements
 
 
                 // Movements
-                rotatoSpeed = Parse(node.GetValue("rotatoSpeed"), rotatoSpeed);
-
-                pivotAround = node.GetValue("pivotAround");
-                pivotPosition = Parse(node.GetValue("pivotPosition"), pivotPosition);
-                pivotRotation = Parse(node.GetValue("pivotRotation"), pivotRotation);
-                pivotScale = Parse(node.GetValue("pivotScale"), pivotScale);
-                pivotDistance = Parse(node.GetValue("pivotDistance"), pivotDistance);
-                pivotRotatoSpeed = Parse(node.GetValue("pivotRotatoSpeed"), pivotRotatoSpeed);
-
-                bobberSeed = Parse(node.GetValue("bobberSeed"), bobberSeed);
-                bobberOFS = Parse(node.GetValue("bobberOFS"), bobberOFS);
+                trackCamera = Parse(node.GetValue("trackCamera"), trackCamera);
             }
 
             // New MenuLight from name
@@ -103,7 +83,7 @@ namespace SigmaReplacements
 
 
             // Apply MenuLight to GameObject
-            internal void ApplyTo(GameObject obj, float scaleMult = 1)
+            internal void ApplyTo(GameObject obj)
             {
                 if (obj == null) return;
                 if (Debug.debug) obj.AddOrGetComponent<LiveDebug>();
@@ -111,8 +91,7 @@ namespace SigmaReplacements
                 // Edit Position/Rotation/Scale
                 obj.transform.position = position ?? obj.transform.position;
                 obj.transform.rotation = rotation ?? obj.transform.rotation;
-                if (scale != null)
-                    obj.transform.localScale = (Vector3)scale * scaleMult;
+                obj.transform.localScale = scale ?? obj.transform.localScale;
 
                 // Edit Appearances
                 if (obj.GetComponent<Light>() is Light light)
@@ -126,74 +105,9 @@ namespace SigmaReplacements
                     light.shadowStrength = shadowStrength ?? light.shadowStrength;
                 }
 
-
-                // Rotato
-                if (rotatoSpeed != null)
+                if (trackCamera == true)
                 {
-                    Rotato rotato = obj.AddOrGetComponent<Rotato>();
-                    rotato.speed = (float)rotatoSpeed;
-                }
-
-
-                // Pivot
-                if (!string.IsNullOrEmpty(pivotAround))
-                {
-                    GameObject scene = obj;
-
-                    while (scene != null && scene.name != "OrbitScene" && scene.name != "MunScene")
-                    {
-                        scene = scene?.transform?.parent?.gameObject;
-                    }
-
-                    if (scene == null) return;
-
-                    GameObject parent = null;
-
-                    if (pivotAround != null)
-                        parent = scene.GetChild(pivotAround);
-
-                    parent = parent ?? obj?.transform?.parent?.gameObject;
-
-
-                    if (parent != null)
-                    {
-                        GameObject pivot = new GameObject(obj.name + "_Pivot");
-
-                        pivot.transform.SetParent(parent.transform);
-
-                        pivot.transform.position = pivotPosition ?? parent.transform.position;
-                        pivot.transform.localRotation = pivotRotation ?? Quaternion.Euler(Vector3.zero);
-                        pivot.transform.localScale = pivotScale ?? Vector3.one;
-
-                        obj.transform.SetParent(pivot.transform);
-
-                        if (pivotDistance != null)
-                        {
-                            obj.transform.localPosition = Vector3.left * (float)pivotDistance;
-                        }
-                    }
-                }
-
-                if (pivotRotatoSpeed != null)
-                {
-                    if (obj?.transform?.parent?.gameObject != null)
-                    {
-                        Rotato pivotRotato = obj.transform.parent.gameObject.AddOrGetComponent<Rotato>();
-                        pivotRotato.speed = (float)pivotRotatoSpeed;
-                    }
-                }
-
-
-                // Bobber
-                if (bobberSeed != null || bobberOFS != null)
-                {
-                    Bobber bobber = obj.AddOrGetComponent<Bobber>();
-
-                    bobber.seed = bobberSeed ?? bobber.seed;
-
-                    bobber.ofs1 = bobberOFS?.x ?? bobber.ofs1;
-                    bobber.ofs2 = bobberOFS?.y ?? bobber.ofs2;
-                    bobber.ofs3 = bobberOFS?.z ?? bobber.ofs3;
+                    obj.AddOrGetComponent<LightTracker>();
                 }
             }
         }
---
+++ [Source]/SigmaReplacements/MenuScenes/Utility/LightTracker.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+
+namespace SigmaReplacements
+{
+    namespace MenuScenes
+    {
+        internal class LightTracker : MonoBehaviour
+        {
+            void Update()
+            {
+                transform.rotation = Quaternion.LookRotation(Camera.main.transform.position - transform.position);
+            }
+        }
+    }
+}
