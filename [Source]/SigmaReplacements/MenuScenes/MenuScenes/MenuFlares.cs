1296947ba56d6376a67fb36fd0826ed993a1fe62
Sigma88 <sigmamodexpansions@outlook.com>
2020-02-18 02:00:42 +0100

    destroy on scene change

--- [Source]/SigmaReplacements/MenuScenes/MenuScenes/MenuFlares.cs
+++ [Source]/SigmaReplacements/MenuScenes/MenuScenes/MenuFlares.cs
@@ -27,6 +27,17 @@ namespace SigmaReplacements
                                    1 << 20 | 1 << 21 | 1 << 22 | 1 << 23 | 1 << 24 | 1 << 25 | 1 << 26 | 1 << 27 | 1 << 28 | 1 << 29 |
                                    1 << 30 | 1 << 31;
 
+            void Start()
+            {
+                GameEvents.onGameSceneSwitchRequested.Add(SceneSwitch);
+            }
+
+            void SceneSwitch(GameEvents.FromToAction<GameScenes, GameScenes> scenes)
+            {
+                DestroyImmediate(flare.gameObject);
+                DestroyImmediate(this);
+            }
+
             void Update()
             {
                 flare.transform.rotation = Quaternion.LookRotation(Camera.main.transform.position - transform.position);
