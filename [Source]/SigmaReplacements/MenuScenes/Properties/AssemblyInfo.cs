8d1ee93c12dd6402c0cec9d3e7671fb745202b92
Sigma88 <sigmamodexpansions@outlook.com>
2020-02-25 16:43:57 +0100

    fix assembly info

--- [Source]/SigmaReplacements/MenuScenes/Properties/AssemblyInfo.cs
+++ [Source]/SigmaReplacements/MenuScenes/Properties/AssemblyInfo.cs
@@ -1,12 +1,12 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyTitle("SigmaReplacementsSkyBox")]
-[assembly: AssemblyDescription("Sigma Replacements: SkyBox")]
+[assembly: AssemblyTitle("SigmaReplacementsMenuScenes")]
+[assembly: AssemblyDescription("Sigma Replacements: MenuScenes")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Sigma88")]
 [assembly: AssemblyProduct("")]
 [assembly: AssemblyCopyright("Copyright (C) Sigma88 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: KSPAssembly("SigmaReplacementsSkyBox", 1, 0)]
+[assembly: KSPAssembly("SigmaReplacementsMenuScenes", 1, 0)]
 [assembly: AssemblyVersion("1.0.0")]
